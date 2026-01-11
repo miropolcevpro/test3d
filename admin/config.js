fb00bd1e1488cb15ac1a620ce79431907a86c4a2
miropolcevpro <miropolcevk@gmail.com>
2026-01-12 00:58:57 +0300

    Commit to main

--- admin/config.js
+++ admin/config.js
@@ -3,4 +3,4 @@
 //   window.API_BASE_URL = "https://<gateway-id>.apigw.yandexcloud.net";
 // Для локального теста:
 //   window.API_BASE_URL = "http://localhost:8787";
-window.API_BASE_URL = window.API_BASE_URL || "http://localhost:8787";
+window.API_BASE_URL = window.API_BASE_URL || "https://d5d1712p9mu7k3aurh9s.laqt4bj7.apigw.yandexcloud.net";
