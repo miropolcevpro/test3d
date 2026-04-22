188b4a743f8605b5bfe6c2f5a07d6b513219952e
miropolcevpro <miropolcevk@gmail.com>
2026-04-22 22:57:47 +0300

    Commit to main

--- calculator_module/config.js
+++ calculator_module/config.js
@@ -1,7 +1,7 @@
 (function(global){
   'use strict';
   global.__AG_CALCULATOR_CONFIG__ = Object.freeze({
-    version: '20260422-f24dv',
+    version: '20260422-f24dw',
     submitMode: 'standalone',
     submitEndpoint: '',
     telegramShareBaseUrl: '',
